bdb237da172c2b0c1ec9c7fa722cf8c84588d873
Your Name <vguerrago@cesde.net>
2023-12-04 15:41:43 -0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Entrada Programa
+        String nombreUsuario="Edward Estiven Lopez Idarraga";
+        short edadUsuario=19;
+        String telefonoUsuario="3137523745";
+        String documentoIdentidad="1109291287";
+        int cuentaEnero;
+        int cuentaFebrero;
+        int cuentaMaezo;
+        int cuentaAbril;
+        int cuentaMayo;
+        int cuentaJunio;
+        int cuentaJulio;
+        int cuentaAgosto;
+        int cuentaSeptiembre;
+        int cuentaOctubre;
+        int cuentaNoviembre;
+        String direccionUsuario="CLL 45# 90-02";
+        double costoInscricion=150000.0;
+        float tasadeInteresNatillera=0.1F;
+       double saldoFinaldelUsuario;
+
+       // Proceso
+
+        //1. Asignar un valor especifico a una variable
+        //desde el teclado (Recibir datos)
+
+        Scanner leerDato=new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu monbre?");
+
+        leerDato.nextLine();
+
+
+    }
+}
